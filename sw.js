2e79328bb11fb3eee52b7dfecc832e3506bf4e0e
blakelowis <53172859+blakelowis@users.noreply.github.com>
2026-07-28 18:02:58 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'birds-store-visit-v4';
-const ASSETS = ['./', './index.html', './manifest.json', './css/style.css?v=2', './js/seed-data.js', './js/app.js', './js/jspdf.umd.min.js'];
+const CACHE = 'birds-store-visit-v5';
+const ASSETS = ['./', './index.html', './manifest.json', './css/style.css?v=2', './js/seed-data.js', './js/app.js'];
 
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))); self.skipWaiting(); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k))))); self.clients.claim(); });
